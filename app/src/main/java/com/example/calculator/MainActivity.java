8f69abdca031c85283e7e8ec74ba45fae1e4494a
Jasper_Son <tnh3113@naver.com>
2021-03-05 12:53:18 +0900

    calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private String contentValue;
+    private int resultValue = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
